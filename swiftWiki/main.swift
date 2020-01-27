cc5a8a98e06a3e32723f8c70481b46e0d9877239
Manpreet Singh <manpreet.singh@cineplex.com>
2020-01-27 15:27:52 -0500

    Initial Commit

---
+++ swiftWiki/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftWiki
+//
+//  Created by Manpreet Singh on 2020-01-27.
+//  Copyright © 2020 Manpreet Singh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
